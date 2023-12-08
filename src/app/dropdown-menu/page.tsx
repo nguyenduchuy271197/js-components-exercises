8f8ee8ff15e230ce136b57cfe1883c1217443438
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2023-12-08 14:06:29 +0700

    update

--- src/app/dropdown-menu/page.tsx
+++ src/app/dropdown-menu/page.tsx
@@ -1,20 +1,29 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { useEffect, useRef, useState } from "react";
+import useOnClickOutside from "@/hooks/use-on-click-outside";
+import { useRef, useState } from "react";
 
 export default function DropdownMenuPage() {
   const [open, setOpen] = useState(false);
-  const ref = useRef<HTMLElement | null>()
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  const handleClickOutside = () => {
+    setOpen(false);
+  };
+
+  useOnClickOutside(ref, handleClickOutside);
 
   return (
     <div className="p-8">
       {/* Dropdown Menu */}
-      <div className="relative">
-        <Button onClick={() => setOpen(true)}>Open</Button>
+      <div className="relative inline-flex" ref={ref}>
+        <Button onClick={() => setOpen(true)} variant="outline">
+          Open
+        </Button>
 
         {/* Dropdown Content */}
         {open && (
-          <div className="min-w-[300px] h-[600px] absolute top-full left-0">
+          <div className="min-w-[300px] absolute top-full left-0">
             <ul className="flex flex-col">
               <li className="p-4 bg-gray-50 border-b"></li>
               <li className="p-4 bg-gray-50 border-b"></li>
--- src/hooks/use-on-click-outside.tsx
+++ src/hooks/use-on-click-outside.tsx
@@ -3,16 +3,17 @@ import { useEffect, MutableRefObject } from "react";
 /**
  * Hook that alerts clicks outside of the passed ref
  */
-export default function useOutsideAlerter(
-  ref: MutableRefObject<HTMLElement | null | undefined>
+export default function useOnClickOutside(
+  ref: MutableRefObject<HTMLElement | null | undefined>,
+  handler: (event: MouseEvent) => void
 ) {
   useEffect(() => {
     /**
      * Alert if clicked on outside of element
      */
     function handleClickOutside(event: MouseEvent) {
-      if (ref.current && !ref.current.contains(event.target)) {
-        alert("You clicked outside of me!");
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        handler(event);
       }
     }
     // Bind the event listener
